0585abc7445a995b632d09186cc3c5d882c8aab4
KellieEll <Kellie.ell@journalism.cuny.edu>
2017-03-24 15:25:47 -0400

    fixed project

--- main.js
+++ main.js
@@ -22,6 +22,29 @@ $(document).ready(function () { // we must wait for the DOM to be ready as the b
 		//add a class="active" to #slide to apply the #slide.active styles to it, making it visible
 	});
 
+	$(".circle").on("click", function (e) {
+		e.preventDefault(); //have to add this so we don't get weird behavior in older browsers (or IE)
+		var circle_clicked = $(this),
+			text = circle_clicked.data("text"); //looks for the data-text attribute and stores its value
+
+
+		// $("#popIn").text(text);
+		//assign the text for the element to the popIn container in the slide
+
+		popUp = circle_clicked.closest('.photo-container').find(".info_slides"),
+			popUp_text = popUp.find(".info_popIn");
+
+		popUp_text.text(text);
+
+
+
+
+		popUp.addClass("active");
+		// $("#slide").addClass("active");
+		//add a class="active" to #slide to apply the #slide.active styles to it, making it visible
+	});
+
+
 
 
 	$(".btn_close").on("click", function (e) {
